67e56765ffa6f03e028bdb0144223fe2e8c35cf4
Alexey Soshin <alexey.soshin@gettaxi.com>
2018-07-27 11:25:11 +0200

    Split main method

--- smali2java.go
+++ smali2java.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"github.com/alexeysoshin/smali2java/parser"
 	"log"
 	"os"
@@ -14,12 +15,17 @@ const smaliExtension = ".smali"
 
 func main() {
 
-	var wg = sync.WaitGroup{}
 	pathToSmali := flag.String("path_to_smali", "./test_data/s.smali", "Path to your smali files")
 
 	flag.Parse()
 
-	filepath.Walk(*pathToSmali, func(path string, f os.FileInfo, err error) error {
+	parseSmaliFiles(*pathToSmali)
+}
+
+func parseSmaliFiles(path string) {
+
+	var wg = sync.WaitGroup{}
+	filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
 
 		if f.IsDir() {
 			//log.Printf("Skipping directory: %s\n", path)
@@ -31,13 +37,17 @@ func main() {
 
 		return nil
 	})
-
 	wg.Wait()
 }
 
 func convertSmali(path string, wg *sync.WaitGroup) {
 
-	log.Printf("Processing %s\n", path)
+	/*defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("%s %v\n", path, r)
+		}
+	}()*/
+	fmt.Printf("Processing %s\n", path)
 	wg.Add(1)
 
 	//go func() {
