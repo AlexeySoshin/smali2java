012e8794fb3b089c116985e75cbaa00ba0212cc9
Steven Swor <sworisbreathing@gmail.com>
2019-12-26 00:18:15 +1100

    Handle const-string values with spaces in them

--- parser/final_string_test.go
+++ parser/final_string_test.go
@@ -19,3 +19,17 @@ func TestFinalString(t *testing.T) {
 
 	assert.Equal(t, expectedOutput, output)
 }
+
+func TestFinalStringWithSpaces(t *testing.T) {
+	input := `const-string v0, "Hello, World!"`
+
+	javaFile := &JavaFile{}
+
+	(&FinalStringParser{}).Parse(javaFile, strings.Fields(input))
+
+	expectedOutput := `final String v0 = "Hello, World!" ; // const-string v0, "Hello, World!"`
+
+	output := strings.Join(javaFile.First(), " ")
+
+	assert.Equal(t, expectedOutput, output)
+}
