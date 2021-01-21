b271b9edaa4b6575152eadf31ad83f8fe62aa5ed
Peter <petzah+github@znik.sk>
2021-01-21 17:54:28 +0000

    increase buffer size to 1M (#18)

--- smali2java.go
+++ smali2java.go
@@ -64,6 +64,8 @@ func convertSmali(path string, wg *sync.WaitGroup) {
 
 	javaFile := parser.JavaFile{}
 	scanner := bufio.NewScanner(file)
+	buf := make([]byte, 0, 64*1024)
+	scanner.Buffer(buf, 1024*1024)
 	for scanner.Scan() {
 		javaFile.ParseLine(scanner.Text())
 	}
